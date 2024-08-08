ba79136c635a6abf302938cb9aaf2965bf260b8c
Someoneslamp <112986645+Someoneslamp@users.noreply.github.com>
2024-08-07 20:59:21 -0700

    Initial

--- src/com/gmail/lampbussinessextra/Classes/Coffee.java
+++ src/com/gmail/lampbussinessextra/Classes/Coffee.java
@@ -63,7 +63,7 @@ public class Coffee {
 
     // public final void brew() {}
 
-    public CoffeeType getCoffeeType() { return COFFEE_TYPE; }
+    public final CoffeeType getCoffeeType() { return COFFEE_TYPE; }
     public final BrewType getBrewType() { return BREW_TYPE; }
     public final Double getSize() { return SIZE; }
 }
